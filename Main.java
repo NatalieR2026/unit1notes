77d3da17407db1145d2714d9461c5812d2008160
NatalieR2026 <natalie.radin2026@gbwl.org>
2024-09-11 13:40:09 +0000

    update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,30 @@
 public class Main {
 
    public static void main(String []args) {
+      // below is a print statement (instruction/command)
+      // java statements end in a semicolon
       System.out.println("Hello World");
+
+      // print will  output on the same line
+      // printIn will  enter to the next line after printing
+      System.out.print("apcs");
+      System.out.print("is fun");
+
+      // declare variables
+      int students;
+      double cash;
+      boolean isRaining; // camelCase for multi-word names
+
+      // assign values to the variables
+      students = 8;
+      cash = 5.00;
+      isRaining = false; // false is "off" or 0
+
+      // instantiate or initialize (declare + assign in one line)
+      int numClasses = 9;
+      double gpa = 3.4;
+      boolean isSnowy = false;
+
+
    }
 }
